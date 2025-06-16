4287bf0e2e32ac48d8b4aef0f3cb4e843f936353
Mehmet Akif <mehmet.akif.20021@gmail.com>
2025-06-16 13:29:03 +0300

    Update

--- main.py
+++ main.py
@@ -220,4 +220,5 @@ async def predict_disease(image: UploadFile = File(...)):
         )
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True) 
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True) 
